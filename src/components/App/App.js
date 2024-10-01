13d5e976625ed2f8ec84e90f36dc2c0bdd648190
VicenteStramantino <vicente.stramantino@germinare.com.br>
2024-10-01 10:01:30 -0300

    subindo tudo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,14 +7,14 @@ function App() {
   return (
       <>
       {/* Decidi grarantir os componentes. Tentei fazer todos eles de forma funcional, ou seja, fiz tudo que acreditei ser componente, a parte a seguir é referente a garantia de 7 */}
-
-      {/* <Carne foto={carneImg} nomeItem="Carne" kilopreco="1Kg/Preco" preço="0000.000"/>
+{/* 
       <Carne foto={carneImg} nomeItem="Carne" kilopreco="1Kg/Preco" preço="0000.000"/>
-      <Carne foto={carneImg} nomeItem="Carne" kilopreco="1Kg/Preco" preço="0000.000"/> */
-      }
+      <Carne foto={carneImg} nomeItem="Carne" kilopreco="1Kg/Preco" preço="0000.000"/>
+      <Carne foto={carneImg} nomeItem="Carne" kilopreco="1Kg/Preco" preço="0000.000"/> */}
+      
 
       {/* A parte a seguir é referente a garantir 10 */}
-      {/* <Carne2 foto={carneImg} nomeItem="Carne" kilopreco="1Kg/Preco" preco="0000.000" tipoCliente="Saudavel"/> */}
+      <Carne2 foto={carneImg} nomeItem="Carne" kilopreco="1Kg/Preco" preco="0000.000" tipoCliente="Natural"/>
       </>
   );
 }
